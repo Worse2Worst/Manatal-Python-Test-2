30e4b27ea409ae76fd10bd98ae9f6c963d341b27
Worse2Worst <wasuwach@hotmail.com>
2022-03-01 18:18:15 +0900

    Fix

--- lottery.py
+++ lottery.py
@@ -1,7 +1,9 @@
 import random
 
-# from 1 to 50
-all_numbers = list(range(1, 51))
-winners = random.sample(all_numbers, 10)
-winners.sort()
-print(f'From numbers 1 - 50, the winning numbers are: {winners}')
+
+def draw_lottery():
+    # numbers is a list contains 1 to 50
+    numbers = list(range(1, 51))
+    winners = random.sample(numbers, 10)
+    winners.sort()
+    return winners
